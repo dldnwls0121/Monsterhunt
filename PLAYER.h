2e22415eeb25f68bd8d4681eb92a2511d5a696b4
dldnwls4148 <dldnwls4148@naver.com>
2025-06-20 02:11:34 +0900

    11

--- PLAYER.h
+++ PLAYER.h
@@ -1,7 +1,27 @@
 #pragma once
+#include "DoubleBuffer.h"
+enum Dir
+{
+	LEFT,
+	RIGHT,
+	LSPACE,
+	RSPACE,
+	DIE,
 
+};
 class PLAYER
 {
-
+private:
+	int x;
+	int y;
+	int hp;
+	int atk;
+	int def;
+	Dir dir;
+	string shape[5][4];
+public:
+	void init();
+	void Progressstage();
+	void Renderstage();
 };
 
